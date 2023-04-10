e47dbbd6c768ed8f5d8eb28a05bad4d1764227a6
Richard Henage <rthenage@gmail.com>
2023-04-10 17:41:47 -0600

    Config Features and Data

--- src/content/homepage.ts
+++ src/content/homepage.ts
@@ -1,17 +1,18 @@
+import { HOME } from "src/config";
 export const homepage = [
   {
-    title: "Blazingly Fast",
-    content: "The reason why I chose Astro is because it's blazingly fast. It's also a component-based framework, so you can easily reuse your components.",
+    title: HOME.TEXT["Feature 1 Title"],
+    content: HOME.TEXT["Feature 1 Text"],
     icon: "charm:rocket",
   },
   {
-    title: "Easy to use",
-    content: "Need to use this template? Just clone it and start writing your content. No complicated setup, no complicated design.",
+    title: HOME.TEXT["Feature 2 Title"],
+    content: HOME.TEXT["Feature 2 Text"],
     icon: "ph:code-bold",
   },
   {
-    title: "Free and Open Source",
-    content: "Found some issues or want to contribute? Feel free to open an issue or pull request on GitHub.",
+    title: HOME.TEXT["Feature 3 Title"],
+    content: HOME.TEXT["Feature 3 Text"],
     icon: "charm:plant-pot",
   },
 ];
